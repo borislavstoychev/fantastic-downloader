85c92d42bd274a23855d6e4b44fece3c644b8eb4
borislavstoychev <stoy4ew@gmail.com>
2021-09-25 23:42:57 +0300

    youtube downloader

---
+++ downloader/downloader/youtube_downloader/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class YoutobeDownloaderConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'downloader.youtube_downloader'
---
+++ downloader/downloader/youtube_downloader/views.py
@@ -0,0 +1,46 @@
+import re
+from pytube import YouTube
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import FileResponse
+from django.utils.encoding import smart_str
+from .forms import DownloadForm
+
+
+def download_video(request):
+    global context
+    form = DownloadForm(request.POST)
+    if form.is_valid():
+        video_url = form.cleaned_data['url']
+        if not re.match(r'^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+', video_url):
+            return HttpResponse('Enter correct url.')
+        video = YouTube(video_url)
+        video_streams = video.streams.get_highest_resolution()
+        audio_streams = video.streams.get_audio_only()
+        context = {
+            'form': DownloadForm(),
+            'title': video.title,
+            'video_streams': video_streams,
+            'video_streams_size': video_streams.filesize/100000,
+            'description': video.description,
+            'rating': video.rating,
+            'views': video.views,
+            'thumb': video.thumbnail_url,
+            'author': video.author,
+            'audio_stream': audio_streams,
+            'audio_stream_size': audio_streams.filesize/100000,
+            'download': video_streams.url + "&title=" + video.title,
+            'download_audio': audio_streams.url + "&title=" + video.title,
+
+        }
+
+        return render(request, 'home.html', context)
+
+    return render(request, 'home.html', {'form': form})
+
+
+# def download(request, pk):
+#     file =
+#     response = FileResponse(open(file.download(), 'rb'), as_attachment=True, filename=title)
+#     return response
+
