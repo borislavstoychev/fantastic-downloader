8b6869441d67e12737c7bf16bb31bb42ca2bee41
borislavstoychev <stoy4ew@gmail.com>
2021-10-14 16:59:01 +0300

    reusable code

--- downloader/youtube_downloader/urls.py
+++ downloader/youtube_downloader/urls.py
@@ -1,8 +1,8 @@
 
 from django.urls import path
-from .views import download_video, download
+from .views import download_home, download_search
 
 urlpatterns = [
-    path('', download_video, name='home'),
-    path('download/', download, name='download'),
+    path('', download_home, name='home'),
+    path('download/', download_search, name='download'),
 ]
--- downloader/youtube_downloader/views.py
+++ downloader/youtube_downloader/views.py
@@ -5,62 +5,43 @@ from django.shortcuts import render
 from .forms import DownloadForm
 
 
-def download_video(request):
-    global context
+def get_context(url):
+    ytb = YouTube(url)
+    video_streams = ytb.streams.get_highest_resolution()
+    audio_streams = ytb.streams.get_audio_only()
+    context = {
+        'form': DownloadForm(),
+        'title': ytb.title,
+        'video_streams': video_streams,
+        'video_streams_size': video_streams.filesize / 1048576,
+        'description': ytb.description,
+        'rating': ytb.rating,
+        'views': ytb.views,
+        'thumb': ytb.thumbnail_url,
+        'author': ytb.author,
+        'audio_stream': audio_streams,
+        'audio_stream_size': audio_streams.filesize / 1048576,
+        'download': video_streams.url + "&title=" + ytb.title,
+        'download_audio': audio_streams.url + "&title=" + ytb.title,
+
+    }
+    return context
+
+
+def download_home(request):
     form = DownloadForm(request.POST)
     if form.is_valid():
         video_url = form.cleaned_data['url']
         if not re.match(r'^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+', video_url):
             return HttpResponse('Enter correct url.')
-        video = YouTube(video_url)
-        video_streams = video.streams.get_highest_resolution()
-        audio_streams = video.streams.get_audio_only()
-        context = {
-            'form': DownloadForm(),
-            'title': video.title,
-            'video_streams': video_streams,
-            'video_streams_size': video_streams.filesize/1048576,
-            'description': video.description,
-            'rating': video.rating,
-            'views': video.views,
-            'thumb': video.thumbnail_url,
-            'author': video.author,
-            'audio_stream': audio_streams,
-            'audio_stream_size': audio_streams.filesize/1048576,
-            'download': video_streams.url + "&title=" + video.title,
-            'download_audio': audio_streams.url + "&title=" + video.title,
-
-        }
-
-        return render(request, 'home.html', context)
-
-    return render(request, 'home.html', {'form': form})
+        return render(request, 'home.html', get_context(video_url))
+    return render(request, 'home.html', {'form': DownloadForm()})
 
 
-def download(request):
+def download_search(request):
     if request.method == "POST":
         url = request.POST['download']
-        video = YouTube(url)
-        video_streams = video.streams.get_highest_resolution()
-        audio_streams = video.streams.get_audio_only()
-        context = {
-            'form': DownloadForm(),
-            'title': video.title,
-            'video_streams': video_streams,
-            'video_streams_size': video_streams.filesize / 1048576,
-            'description': video.description,
-            'rating': video.rating,
-            'views': video.views,
-            'thumb': video.thumbnail_url,
-            'author': video.author,
-            'audio_stream': audio_streams,
-            'audio_stream_size': audio_streams.filesize / 1048576,
-            'download': video_streams.url + "&title=" + video.title,
-            'download_audio': audio_streams.url + "&title=" + video.title,
-
-        }
-
-        return render(request, 'home.html', context)
-    return render(request, 'youtube_search.html')
+        return render(request, 'home.html', get_context(url))
+    return render(request, 'home.html', {'form': DownloadForm()})
 
 
